1e26a4f46bdd2a24e6d0b53909fd61400a62bba7
Enric Jordà <enric@pati.io>
2019-07-02 21:55:52 +0200

    more than 3 neighbours dies

--- src/com/tdd/gameOfLife/GameOfLife.java
+++ src/com/tdd/gameOfLife/GameOfLife.java
@@ -12,16 +12,43 @@ public class GameOfLife {
 
     public void nextGen() {
 
-        this.board[0][0] = 0;
+        int[][] newBoard = new int [this.board.length][this.board[0].length];
 
         for(int i = 0, len = this.board.length; i< len; ++i) {
             for(int j = 0, len2 = this.board[i].length; j< len2; ++j) {
 
-                this.board[i][j] = (this.board[i][j] == 1) ? 0 : this.board[i][j];
+                newBoard[i][j] = (getAmountOfNeighbours(i,j) < 2) ? 0 : this.board[i][j];
 
             }
         }
 
+        this.board = newBoard;
+
+    }
+
+    private int getAmountOfNeighbours(int x, int y) {
+
+        int totalNeighbours = 0;
+
+        totalNeighbours += getValueOf(x-1,y-1);
+        totalNeighbours += getValueOf(x,y-1);
+        totalNeighbours += getValueOf(x+1,y-1);
+
+        totalNeighbours += getValueOf(x-1,y);
+        totalNeighbours += getValueOf(x+1,y);
+
+        totalNeighbours += getValueOf(x-1,y+1);
+        totalNeighbours += getValueOf(x,y+1);
+        totalNeighbours += getValueOf(x+1,y+1);
+
+        return totalNeighbours;
+    }
+
+    private int getValueOf(int x, int y) {
+        if(x<0 || y < 0) return 0;
+        if(x>=this.board.length || y>= this.board.length) return 0;
+
+        return this.board[x][y];
     }
 
     public int[][] getBoard() {
--- test/com/tdd/gameOfLife/GameOfLifeShould.java
+++ test/com/tdd/gameOfLife/GameOfLifeShould.java
@@ -68,4 +68,29 @@ public class GameOfLifeShould {
         Assertions.assertArrayEquals(expectedGrid,
                 gameOfLife.getBoard());
     }
+
+    @Test
+    public void cell_with_more_than_3_neighbours_dies() {
+
+        int [][] grid =
+                {
+                        {1,1,1},
+                        {1,0,0},
+                        {1,1,0}
+                };
+
+        GameOfLife gameOfLife = new GameOfLife(grid);
+        gameOfLife.nextGen();
+
+        int [][] expectedGrid =
+                {
+                        {1,1,0},
+                        {0,0,0},
+                        {1,1,0}
+                };
+
+
+        Assertions.assertArrayEquals(expectedGrid,
+                gameOfLife.getBoard());
+    }
 }
